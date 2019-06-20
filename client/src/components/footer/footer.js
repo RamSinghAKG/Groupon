0633e931f20294c2785bf904b671a672aa05806e
RamSinghAKG <ram.singh.akg@gmail.com>
2019-06-20 07:24:19 +0530

    Updated footer

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -6,7 +6,6 @@ const Footer = (props) => {
     return (
         <ErrorBoundary>
             <footer className="footer-container">
-                    <hr></hr>
                     <a aria-label="go to about page" target="_blank" rel="noopener noreferrer" className="about" href="https://www.groupon.com/press/about-groupon">About Groupon</a>
             </footer>
         </ErrorBoundary>
